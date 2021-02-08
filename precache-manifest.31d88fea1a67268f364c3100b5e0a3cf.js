bf50c9d363972073e0bbe16a5d9337f0a989d90c
mojoman353 <mojoman353@gmail.com>
2021-02-09 00:57:59 +0530

    portfolio

---
+++ precache-manifest.31d88fea1a67268f364c3100b5e0a3cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "226646756d4233af1ed7",
+    "url": "/static/css/main.12e950af.chunk.css"
+  },
+  {
+    "revision": "226646756d4233af1ed7",
+    "url": "/static/js/main.3165ec32.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/static/js/2.d8e5f684.chunk.js"
+  },
+  {
+    "revision": "723218c32284717dd87ae993fd15cb13",
+    "url": "/index.html"
+  }
+];
